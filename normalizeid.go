6741bab75af07bb9c9a92e1eb92bcb4c950eda57
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-05-02 09:41:47 -0700

    xrpcid.NormalizeID()

---
+++ normalizeid.go
@@ -0,0 +1,21 @@
+package xrpcuri
+
+import (
+	"github.com/reiver/go-nsid"
+)
+
+// NormalizeID returns the normalized form of an XRPC-URI 'id', as defined in:
+// https://atproto.com/specs/xrpc
+//
+// An XRPC-URI 'id' is an NSID, as defined at:
+// https://atproto.com/specs/nsid
+//
+// The XRPC-URI 'id' (such as "com.example.fooBar") is part of an XRPC-URI (such as "xrpc://archive.org/com.example.fooBar").
+//
+// An example of a non-normalized XRPC-URI 'id' would be "COM.Example.fooBar".
+// Normalizing that non-normalized XRPC-URI 'id' would result in "com.example.fooBar".
+//
+// Note that if you want to normalize a whole XRPC-URI rather than just a 'id', then instead use [Normalize].
+func NormalizeID(value string) string {
+	return nsid.Normalize(value)
+}
---
+++ normalizeid_example_test.go
@@ -0,0 +1,21 @@
+package xrpcuri_test
+
+import (
+	"fmt"
+
+	"github.com/reiver/go-xrpcuri"
+)
+
+func ExampleNormalizeID() {
+
+	var id string = "COM.Example.fooBar"
+
+	normalizedID := xrpcuri.NormalizeID(authority)
+
+	fmt.Printf("original id:   %s\n", id)
+	fmt.Printf("normalized id: %s\n", normalizedID)
+
+	// Output:
+	// original id:   COM.Example.fooBar
+	// normalized id: com.example.fooBar
+}
